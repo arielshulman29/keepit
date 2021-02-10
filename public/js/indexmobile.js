20b267bbbe620201bcb53fc6a74d42df40916ae1
arielshulman29 <73914404+arielshulman29@users.noreply.github.com>
2021-02-10 18:07:00 +1000

    no zoom on mobile on dblclick

--- public/js/indexmobile.js
+++ public/js/indexmobile.js
@@ -23,25 +23,7 @@ $('a.room').click(function () {
         }
     }
 });
-$('.add').click(addClick());
-$('.add').dblclick(addClick());
-
-$('.sub').click(subClick());
-$('.sub').dblclick(subClick());
-
-$('.hide-room').click(function () {
-    let id = $(this).attr('id');
-    $('#' + id + '.products').css('display', 'none');
-    $('a#' + id).removeClass('active');
-    $('a#' + id + ' i').removeClass('fa-minus');
-    $('a#' + id + ' i').addClass('fa-plus');;
-});
-function toggleIcon(id) {
-    let linkSelector = 'a.room#' + id + ' .toggle-icon i';
-    $(linkSelector).toggleClass('fa-plus');
-    $(linkSelector).toggleClass('fa-minus');
-}
-function addClick() {
+$('.add').click(function () {
     let idSelector = '#' + $(this).parent().attr('id');
     let quantitySelector = idSelector + ' input[type=button]';
     let areaSelector = idSelector + ' input[type=hidden][name=area]'
@@ -80,8 +62,49 @@ function addClick() {
         CurrentPrice = CurrentPrice + price;
         return CurrentPrice;
     });
-}
-function subClick() {
+});
+$('.add').dblclick(function () {
+    let idSelector = '#' + $(this).parent().attr('id');
+    let quantitySelector = idSelector + ' input[type=button]';
+    let areaSelector = idSelector + ' input[type=hidden][name=area]'
+    let priceSelector = idSelector + ' input[type=hidden][name=price]';
+    let roomSelector = idSelector + ' input[type=hidden][name=room]';
+    var currentQuantity = Number($(quantitySelector).val());
+    var area = Number($(areaSelector).val());
+    var price = Number($(priceSelector).val());
+    var roomId = $(roomSelector).val();
+    // add quantity
+    $(quantitySelector).val(function () {
+        currentQuantity = currentQuantity + 1;
+        return currentQuantity
+    })
+    let totalAreasQuery = 'a#' + roomId + ' span span.area, .sum-area-value';
+    let totalPriceQuery = 'a#' + roomId + ' input[type=hidden][name=price-per-room]';
+    // add area 
+    $(totalAreasQuery).text(function () {
+        var currentArea = Number($(this).text());
+        currentArea = currentArea + area;
+        return currentArea;
+    });
+    // add price 
+    $(totalPriceQuery).val(function () {
+        var CurrentPrice = Number($(this).val());
+        CurrentPrice = CurrentPrice + price;
+        return CurrentPrice;
+    });
+    //show added price
+    $('.show-price-per-room').text(function () {
+        return $(totalPriceQuery).val();
+    })
+    // add price to total
+    $('.sum-price-value').text(function () {
+        var CurrentPrice = Number($(this).text());
+        CurrentPrice = CurrentPrice + price;
+        return CurrentPrice;
+    });
+});
+
+$('.sub').click(function () {
     let idSelector = '#' + $(this).parent().attr('id');
     let quantitySelector = idSelector + ' input[type=button]';
     let areaSelector = idSelector + ' input[type=hidden][name=area]'
@@ -122,4 +145,19 @@ function subClick() {
             return CurrentPrice;
         });
     }
+});
+$('.hide-room').click(function () {
+    let id = $(this).attr('id');
+    $('#' + id + '.products').css('display', 'none');
+    $('a#' + id).removeClass('active');
+    $('a#' + id + ' i').removeClass('fa-minus');
+    $('a#' + id + ' i').addClass('fa-plus');;
+});
+function toggleIcon(id) {
+    let linkSelector = 'a.room#' + id + ' .toggle-icon i';
+    $(linkSelector).toggleClass('fa-plus');
+    $(linkSelector).toggleClass('fa-minus');
+}
+function addClick(){
+    
 }
