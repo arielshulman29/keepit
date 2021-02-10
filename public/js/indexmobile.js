dfcbc6b2ef4bb9da4b4b034a4d20cb5314f7d44d
arielshulman29 <73914404+arielshulman29@users.noreply.github.com>
2021-02-10 18:11:55 +1000

    Add files via upload

--- public/js/indexmobile.js
+++ public/js/indexmobile.js
@@ -146,6 +146,49 @@ $('.sub').click(function () {
         });
     }
 });
+
+$('.sub').dblclick(function () {
+    let idSelector = '#' + $(this).parent().attr('id');
+    let quantitySelector = idSelector + ' input[type=button]';
+    let areaSelector = idSelector + ' input[type=hidden][name=area]'
+    let priceSelector = idSelector + ' input[type=hidden][name=price]';
+    let roomSelector = idSelector + ' input[type=hidden][name=room]';
+    var currentQuantity = Number($(quantitySelector).val());
+    var area = Number($(areaSelector).val());
+    var price = Number($(priceSelector).val());
+    var roomId = $(roomSelector).val();
+    if (currentQuantity > 0) {
+        // subtract quantity
+        $(quantitySelector).val(function () {
+            currentQuantity = currentQuantity - 1;
+            return currentQuantity
+        })
+        let totalAreasQuery = 'a#' + roomId + ' span span.area, .sum-area-value';
+        let totalPriceQuery = 'a#' + roomId + ' input[type=hidden][name=price-per-room]';
+        // subtract area 
+        $(totalAreasQuery).text(function () {
+            var currentArea = Number($(this).text());
+            currentArea = currentArea - area;
+            return currentArea;
+        });
+        // subtract price 
+        $(totalPriceQuery).val(function () {
+            var CurrentPrice = Number($(this).val());
+            CurrentPrice = CurrentPrice - price;
+            return CurrentPrice;
+        });
+        //show subtracted price
+        $('.show-price-per-room').text(function () {
+            return $(totalPriceQuery).val();
+        })
+        // subtract price from total
+        $('.sum-price-value').text(function () {
+            var CurrentPrice = Number($(this).text());
+            CurrentPrice = CurrentPrice - price;
+            return CurrentPrice;
+        });
+    }
+});
 $('.hide-room').click(function () {
     let id = $(this).attr('id');
     $('#' + id + '.products').css('display', 'none');
