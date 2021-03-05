694efdc01c5d8b2bbf3ca295cd86faa55105e00a
arielshulman29 <73914404+arielshulman29@users.noreply.github.com>
2021-03-05 11:52:46 +1000

    add user login

--- middleware.js
+++ middleware.js
@@ -1,9 +1,6 @@
 const { furnitureSchema, roomSchema } = require('./Schemas.js');
 const ExpressError = require('./utils/ExpressError');
-const Furniture = require('./models/furniture');
-const Room = require('./models/room');
 
-//are the data we got for the campground valid
 module.exports.validateFurniture = (req, res, next) => {
     const { error } = furnitureSchema.validate(req.body);
     if (error) {
@@ -27,11 +24,34 @@ module.exports.validateRoom = (req, res, next) => {
     }
 }
 
+
+//is the user logged in
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        //in order to redirect the user to the address that he didn't have authorization for after he signs in
+        req.session.returnTo = req.originalUrl;
+        return res.redirect('user/login');
+    }
+    next();
+}
+
+
+//is the user is the admin
+module.exports.isAdmin = async (req, res, next) => {
+    const id = req.user._id;
+    const admin = await User.findOne({ _id: id, isAdmin: true })
+    if (!admin) {
+        req.flash('error', 'אינך מנהל');
+        return res.redirect('/edit');
+    }
+    next();
+}
+
 module.exports.isMobile = (req, res, next) => {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(req.headers["user-agent"])){
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(req.headers["user-agent"])) {
         res.redirect("/mobilemenu");
     }
-    else{
+    else {
         next();
     }
 }
